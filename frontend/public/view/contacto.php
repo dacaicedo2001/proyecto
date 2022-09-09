40250bd46c09c51374cb2d8b941b4e4f773157b1
Daniel Caicedo Murillo <dacaicedo2001@gmail.com>
2022-09-09 16:35:45 -0500

    Pendiente api de google map y estilos css

--- frontend/public/view/contacto.php
+++ frontend/public/view/contacto.php
@@ -22,5 +22,33 @@
             ?>
             <hr>
         </header>
+        <main>
+            <p>Puede contactarse con nosotros para más información, preguntar y asesorarce sobre algún tema de su interés o despejar la inquietud que necesite.</p>
+            <div class="datos">
+                <p>horarios de atención: 9:00 am a 6:00 pm</p>
+                <p>días: de lunes a viernes</p>
+                <p>dirección: cra 1 calle 1</p>
+                <p>correo: ejemplo@ejemplo.com</p>
+                <p>telefono: 123</p>
+                <p>ciudad: buenaventura</p>
+            </div>
+            <form>
+                <h3>déjanos saber quién eres</h3>
+                <div>
+                    <label for="nombre">nombres:</label>
+                    <input type="text" id="nombre" placeholder="Ingrese sus nombres" required>
+                </div>
+                <div>
+                    <label for="correo">correo:</label>
+                    <input type="email" id="correo" placeholder="Ingrese su correo" required>
+                </div>
+                <div>
+                    <label for="comentario">dejanos tu comentario:</label>
+                    <textarea id="comentario" rows="10" placeholder="¿En qué podemos ayudarte?"></textarea>
+                </div>
+                <input type="submit" value="enviar">
+            </form>
+        </main>
+        <?php include_once("../../src/components/footer.php"); ?>
     </body>
 </html>
